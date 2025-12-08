ff8bf8e6952e4c5650d19f8eb7677a57c9981ab0
Deepak S <deepaksenthil673@gmail.com>
2025-12-08 23:52:59 +0530

    Corrected backend: added collection name and cleaned routes

--- models/Menu.js
+++ models/Menu.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-
+const Menu = require("./models/Menu"); 
 const menuSchema = new mongoose.Schema({
   day: { type: String, required: true },
   meal: { type: String, required: true },
   items: { type: String, required: true }, // comma-separated items
 });
 
-module.exports = mongoose.model("menus", menuSchema, "menus"); // third param = collection name
+module.exports = mongoose.model("Menu", menuSchema, "Menu"); 
 
